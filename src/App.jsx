6ac9df8c66e7a3517e287c7f3f55631ecccfc258
Ashley Dsouza <ashleybdsouza@d23-16-166-134.bchsia.telus.net>
2026-01-25 19:44:30 -0800

    Initialize React Site with Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Ashley Dsouza</h1>
+      <p>Technical Program Manager</p>
+    </main>
+  );
+}
+
+export default App;
