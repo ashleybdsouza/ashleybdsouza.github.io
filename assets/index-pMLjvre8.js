9b6d3ae13fb25e34b952db46dd3bb0c495ef8eb1
Ashley Dsouza <ashleybdsouza@ashleys-mbp.lan>
2026-05-14 21:49:33 -0700

    Updates

--- assets/index-pMLjvre8.js
+++ assets/index-pMLjvre8.js
@@ -1999,7 +1999,7 @@ title: "Risk Workbench"
 slug: "risk-workbench"
 date: "2026-05-14"
 excerpt: "Analyzing Risky transactions or onboarding using AI."
-thumbnail: "/images/weather-logo.png"
+thumbnail: "/images/risk-workbench.png"
 demo: "https://ashleybdsouza.github.io/risk-workbench/"
 source: "https://github.com/ashleybdsouza/risk-workbench"
 tags: ["AI","HTML", "CSS", "Javascript", "Charts"]
